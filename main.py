6ee127fc2431156fc0223eb42a3744ff564ff740
quriu <rusinovae025@gmail.com>
2026-03-11 05:32:54 +0400

    Фильтр Собеля

--- main.py
+++ main.py
@@ -21,7 +21,13 @@ def save_np(directory, file_name, np_array):
     image = Image.fromarray(np_array)
     image.save(array_path)
 
-def gauss(np_array, core):
+def gauss(np_array):
+    core = np.array([
+        [1,2,1],
+        [2,4,2],
+        [1,2,1]
+    ])
+
     np_result = np.zeros_like(np_array)
 
     for y in range(1, np_array.shape[0] - 1):
@@ -30,7 +36,26 @@ def gauss(np_array, core):
                 np_result[y,x] = np.sum(window, axis=(0,1)) / np.sum(core)
     return np.clip(np_result, 0, 255).astype(np.uint8)
 
+def sobel(np_array):
+    g_v = np.array([
+        [-1,-2,-1],
+        [0,0,0],
+        [1,2,1]
+    ])
+    g_h = np.array([
+        [-1,0,1],
+        [-2,0,2],
+        [-1,0,1]
+    ])
 
+    np_array = halftone(np_array)
+    np_result = np.zeros_like(np_array)
+
+    for y in range(1, np_array.shape[0] - 1):
+        for x in range(1,np_array.shape[1] - 1):
+                window = np_array[y-1:y+2, x-1:x+2]
+                np_result[y,x] = np.sqrt(np.sum((window * g_h)**2 + (window * g_v)**2))
+    return np.clip(np_result, 0, 255).astype(np.uint8)
 
 paintings = []
 api_url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'
@@ -67,9 +92,8 @@ np_image = np.array(Image.open(image_path).convert('RGB'))
 halftone_np_image = halftone(np_image)
 save_np(directory, 'halftone_image.jpg', halftone_np_image)
 
-gauss_np_image = gauss(np_image, np.array([
-    [1,2,1],
-    [2,4,2],
-    [1,2,1]
-]))
-save_np(directory,'gauss_image.jpg', gauss_np_image)
+#gauss_np_image = gauss(np_image)
+#save_np(directory,'gauss_image.jpg', gauss_np_image)
+
+sobel_np_image = sobel(np_image)
+save_np(directory,'sobel_image.jpg', sobel_np_image)
